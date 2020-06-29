b1ad94cab4c7e4916e0bc5b9fb5713f7a80030d2
Kyle Leonard <kdleonard@icloud.com>
2020-06-28 19:59:32 -0500

    Migrations

---
+++ data/migrations/20200628194927_create-projects.js
@@ -0,0 +1,9 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("Projects", (tbl) => {
+    tbl.increments();
+    tbl.text("name", 128).unique().notNullable();
+    tbl.decimal("budget").notNullable;
+  });
+};
+
+exports.down = function (knex) {};
