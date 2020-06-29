f1a74c38f580ab9e5c49a617a6bbb8c11fb7785c
Kyle Leonard <kdleonard@icloud.com>
2020-06-28 20:34:51 -0500

    Created all needed tables

--- data/migrations/20200628194927_create-projects.js
+++ data/migrations/20200628194927_create-projects.js
@@ -1,9 +1,48 @@
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable("Projects", (tbl) => {
-    tbl.increments();
-    tbl.text("name", 128).unique().notNullable();
-    tbl.decimal("budget").notNullable;
-  });
+  return knex.schema
+
+    .createTable("Projects", (tbl) => {
+      tbl.increments("id").unique();
+      tbl.string("name", 255).notNullable();
+      tbl.string("description", 255);
+      tbl.boolean("Completed").notNullable().defaultTo(false);
+    })
+
+    .createTable("Resources", (tbl) => {
+      tbl.increments("id").unique();
+      tbl.string("name", 255).notNullable();
+      tbl.string("description", 255);
+      tbl
+        .integer("projects_id")
+        .unsigned()
+        .references("projects.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    })
+
+    .createTable("Tasks", (tbl) => {
+      tbl.increments("id").unique();
+      tbl.string("description", 255).notNullable();
+      tbl.string("notes", 255).notNullable();
+      tbl.boolean("Completed").notNullable().defaultTo(false);
+    })
+    .createTable("project_resources", (tbl) => {
+      tbl.increments("id");
+      //foreign keys
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .references("project.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("resources_id")
+        .unsigned()
+        .references("resources.id")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl.string("name", 255).notNullable();
+    });
 };
 
 exports.down = function (knex) {
