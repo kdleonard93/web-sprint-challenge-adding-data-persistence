da7c8466e026a6f863e01eab40cef47108c9bb18
Kyle Leonard <kdleonard@icloud.com>
2020-09-12 10:23:31 -0400

    Updates to the migrations file

--- data/migrations/20200830185756_projects.js
+++ data/migrations/20200830185756_projects.js
@@ -49,8 +49,8 @@ exports.up = async function (knex) {
 };
 
 exports.down = async function (knex) {
-  await knex.schema.dropTableIfExists("project_resources");
   await knex.schema.dropTableIfExists("tasks");
   await knex.schema.dropTableIfExists("resources");
   await knex.schema.dropTableIfExists("projects");
+  await knex.schema.dropTableIfExists("project_resources");
 };
